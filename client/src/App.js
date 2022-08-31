f8d77c24cc9d2c936a86ad2a5e1e3ac46c219946
Leslie <mosordael05@gmail.com>
2022-08-31 08:25:30 +0200

    creation de notre action à l'aide de useDispatch et du Hook useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 
-import React from 'react'
+import React, { useEffect } from 'react';
 /** nous allons importer tous les composants de material ui que nous allons utilisés */
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+/** envoyer une action  */
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.jpg';
@@ -10,6 +13,12 @@ import useStyles from './styles';
 
 function App() {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts);
+    }, [dispatch]);
+
     return (
         /** pour tout contenir */
         <Container maxWidth="lg">
